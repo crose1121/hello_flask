f7ae0debed0b839b3da2c573c49233febeb898fc
crose1121 <coreycoding1121@outlook.com>
2021-12-03 16:51:58 -0500

    added more flask and jinja functionality

--- server.py
+++ server.py
@@ -2,17 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello From Flask!! This is my very first web server'
+def index():
+    return render_template("index.html", phrase="Please enter your phrase in the URL above preceeded by a forward slash --> /   (i like the phrase oogabooga but that's just me)", num="Please also enter the number of times you want the phrase to repeat, preceeded by a forward slash --> /")
 
 @app.route('/success')
 def success():
     return "SUCCESS!!!!!!"
 
-@app.route('/hello/<string:banana>/<int:num>')
-def hello(banana,num):
-    return f"Hello {banana}"*num
-
+@app.route('/<string:phrase>/<int:num>')
+def hello(phrase,num):
+    return render_template("hello.html",phrase=phrase,num=num)
 
 if __name__ == '__main__':
     app.run(debug=True)
