449ef1305a96b6ae9ff25b70504cd7dd1cc79329
crose1121 <coreycoding1121@outlook.com>
2021-12-03 13:30:58 -0500

    some routing practice

--- server.py
+++ server.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello From Flask!! This is my very first web server'
 
+@app.route('/success')
+def success():
+    return "SUCCESS!!!!!!"
+
+@app.route('/hello/<string:banana>/<int:num>')
+def hello(banana,num):
+    return f"Hello {banana}"*num
+
+
 if __name__ == '__main__':
     app.run(debug=True)
